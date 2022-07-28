f9fed816b7d3f3abffd18f1c72d0abb8f870994f
kevin ernst <anno10@freenet.de>
2022-07-28 20:01:51 +0200

    Added basic layout for Gui system

--- Playground/hdr/gfx/renderer.h
+++ Playground/hdr/gfx/renderer.h
@@ -39,6 +39,29 @@ namespace GFX
 
         namespace Gui
         {
+            struct GuiSettings
+            {
+            public:
+                float lineWidth = 0.01;
+                glm::vec4 windowColor = { 36.0f, 44.0f, 56.0f, 255.0f};
+                glm::vec4 lineColor = {22.0f, 2.0f, 94.0f, 255.0f};
+            };
+
+            struct GuiObject
+            {
+            public:
+                GuiSettings mGuiSettings;
+
+            public:
+                bool DrawButton(const ui32& x, const ui32& y,
+                                const ui32& width, const ui32& height,
+                                const char* text);
+                bool DrawButton(const ui32& x, const ui32& y,
+                                const ui32& width, const ui32& height,
+                                const Image& image, const char* text);
+            };
+
+            GuiObject* CreateGuiObject();
         }
     }
 }
