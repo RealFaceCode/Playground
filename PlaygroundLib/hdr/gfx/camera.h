84c083d484f8a9d0c1612d416dd5fad4a28bddf3
RealFaceCode <anno10@freenet.de>
2022-12-16 20:24:34 +0100

    Implemented camera and Ortho camera

--- PlaygroundLib/hdr/gfx/camera.h
+++ PlaygroundLib/hdr/gfx/camera.h
@@ -1,15 +1,50 @@
 #ifndef PLAYGROUNDLIB_CAMERA_H
 #define PLAYGROUNDLIB_CAMERA_H
-#include <glm.hpp>
+#include "../../hdr/core.h"
 
-struct Camera
-        {
-        public:
-            Camera();
-            ~Camera();
-        private:
-            glm::mat4 mProj;
-            glm::mat4 mView;
-        };
+namespace GFX
+{
+    struct Camera
+    {
+    public:
+        Camera();
+        Camera(const ui32* windowWidth , const ui32* windowHeight);
+        ~Camera();
 
+        virtual void calculate() = 0;
+
+        float getAspect();
+        glm::mat4& getProjectionMatrix();
+        glm::mat4& getViewMatrix();
+    private:
+        glm::mat4 mProj;
+        glm::mat4 mView;
+        ui32* mWindowWidth;
+        ui32* mWindowHeight;
+    };
+
+    struct OrthoCamera : Camera
+    {
+    public:
+        OrthoCamera();
+        OrthoCamera(const ui32* mWindowWidth,
+                    const ui32* mWindowHeight,
+                    const float& left,
+                    const float& right,
+                    const float& top,
+                    const float& bottom,
+                    const float& zNear,
+                    const float& zFar);
+        ~OrthoCamera();
+
+        void calculate() override;
+    private:
+        float mLeft;
+        float mRight;
+        float mTop;
+        float mBottom;
+        float mZNear;
+        float mZFar;
+    };
+}
 #endif //PLAYGROUNDLIB_CAMERA_H
--- PlaygroundLib/src/gfx/camera.cpp
+++ PlaygroundLib/src/gfx/camera.cpp
@@ -1,11 +1,59 @@
 #include "../../hdr/gfx/camera.h"
 
-Camera::Camera()
+namespace GFX
 {
+    Camera::Camera()
+        : mProj(glm::mat4(1.0f)), mView(glm::mat4(1.0f)), mWindowWidth(nullptr), mWindowHeight(nullptr)
+    {}
 
-}
+    Camera::Camera(const ui32 *windowWidth, const ui32 *windowHeight)
+    : mProj(glm::mat4(1.0f)), mView(glm::mat4(1.0f)),
+    mWindowWidth(const_cast<ui32 *>(windowWidth)),
+    mWindowHeight(const_cast<ui32 *>(windowHeight))
+    {}
 
-Camera::~Camera()
-{
+    Camera::~Camera()
+    {}
 
+    float Camera::getAspect()
+    {
+        return (float)*mWindowWidth/(float)*mWindowHeight;
+    }
+
+    glm::mat4 &Camera::getProjectionMatrix()
+    {
+        return mProj;
+    }
+
+    glm::mat4 &Camera::getViewMatrix()
+    {
+        return mView;
+    }
+
+    OrthoCamera::OrthoCamera()
+        : Camera(), mLeft(0.f), mRight(0.f), mTop(0.f), mBottom(0.f), mZNear(0.f), mZFar(0.f)
+    {}
+
+    OrthoCamera::OrthoCamera(const ui32* mWindowWidth,
+                             const ui32* mWindowHeight,
+                             const float& left,
+                             const float& right,
+                             const float& top,
+                             const float& bottom,
+                             const float& zNear,
+                             const float& zFar)
+        : Camera(mWindowWidth, mWindowHeight), mLeft(left), mRight(right), mTop(top), mBottom(bottom), mZNear(zNear), mZFar(zFar)
+    {}
+
+    OrthoCamera::~OrthoCamera()
+    {}
+
+    void OrthoCamera::calculate()
+    {
+        float aspect = getAspect();
+        glm::mat4& proj = getProjectionMatrix();
+        proj = glm::ortho(mLeft * aspect, mRight * aspect, mBottom, mTop, mZNear, mZFar);
+        glm::mat4& view = getViewMatrix();
+        view = glm::lookAt(glm::vec3{0.0f, 0.0f, 0.0f}, glm::vec3{ 0.0f, 0.0f, -1.0f }, glm::vec3{ 0.0f, 1.0f, 0.0f });
+    }
 }
